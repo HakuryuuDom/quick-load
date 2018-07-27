d68b1cd108814741c8b1ce5a04f56ede5fb0b5e0
HakuryuuDom <abohn49@yahoo.com>
2018-07-27 17:12:22 -0400

    clean up

--- setting_migration.js
+++ setting_migration.js
@@ -19,18 +19,15 @@ module.exports = function MigrateSetting(from_ver, to_ver, settings) {
 	if(from_ver === undefined) {
 		return Object.assign(Object.assign({}, DefaultSettings), settings);
 	} else if(from_ver === null) {
-			return DefaultSettings;
+		return DefaultSettings;
 	} else {
-		        // Migrate from older version (using the new system) to latest one
+		
         if (from_ver + 1 < to_ver) {
-            // Recursively upgrade in one-version steps
+
             settings = MigrateSettings(from_ver, from_ver + 1, settings);
             return MigrateSettings(from_ver + 1, to_ver, settings);
         }
-        
-        // If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
-        // a switch for each version step that upgrades to the next version. This enables us to
-        // upgrade from any version to the latest version without additional effort!
+
         switch(to_ver)
         {
             case 2:
