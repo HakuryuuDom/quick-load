26aab6712917d900756563785c7f4123753b8747
HakuryuuDom <abohn49@yahoo.com>
2018-07-27 13:11:24 -0400

    migration fix

--- setting_migration.js
+++ setting_migration.js
@@ -17,11 +17,28 @@ let DefaultSettings = {
 
 module.exports = function MigrateSetting(from_ver, to_ver, settings) {
 	if(from_ver === undefined) {
-		settings.enabled = true;
 		return Object.assign(Object.assign({}, DefaultSettings), settings);
 	} else if(from_ver === null) {
 			return DefaultSettings;
 	} else {
-		throw new Error('So far there is only one settings version and this should never be reached!');
+		        // Migrate from older version (using the new system) to latest one
+        if (from_ver + 1 < to_ver) {
+            // Recursively upgrade in one-version steps
+            settings = MigrateSettings(from_ver, from_ver + 1, settings);
+            return MigrateSettings(from_ver + 1, to_ver, settings);
+        }
+        
+        // If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
+        // a switch for each version step that upgrades to the next version. This enables us to
+        // upgrade from any version to the latest version without additional effort!
+        switch(to_ver)
+        {
+            case 2:
+             settings.enabled = true;
+             break;
+        }
+        
+        return settings;
+
 	}
 }
